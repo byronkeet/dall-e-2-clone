7df1e86eca3000a2691b21f84f4fc1e46d9a7f5c
Byron Keet <keetbis@gmail.com>
2023-03-06 21:23:27 +0200

    Added useEffect to pull in all community images saved in DB

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,23 @@ const Home = () => {
 	const [allPosts, setAllPosts] = useState(null);
 	const [searchText, setSearchText] = useState('');
 
+	useEffect(() => {
+		const fetchPosts = async () => {
+			try {
+				setLoading(true);
+				const response = await fetch('http://localhost:8080/api/v1/posts');
+				const data = await response.json();
+				setAllPosts(data.data.reverse());
+			} catch (error) {
+				console.error(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchPosts();
+	}, []);
+
 	return (
 		<section className="max-w-7xl mx-auto">
 			<div>
@@ -34,7 +51,7 @@ const Home = () => {
 							{searchText ? (
 								<RenderCards data={[]} title="No search results found" />
 							) : (
-								<RenderCards data={[]} title="No posts found" />
+								<RenderCards data={allPosts} title="No posts found" />
 							)}
 						</div>
 					</Fragment>
